c60b2c6e8ff668c8dea04707cf94f7c229a624f3
d3vilman88 <faizar.rahman88@gmail.com>
2020-04-19 15:41:23 +0700

    add display status screen, tittle screen and help menu

--- Display.py
+++ Display.py
@@ -1,3 +1,8 @@
+import os
+import time
+import sys
+from Hangman import main
+
 
 def display_hangman(tries):
     stages = [  # final state: head, torso, both arms, and both legs
@@ -87,10 +92,53 @@ def display_hangman(tries):
     ]
     return stages[tries]
 
-def tittle_scrn():
-    pass
 
-def status_scrn(): 
-    print("====================")
-    print(f"=== nyawa = {tries} ")
-    print("====================")
+def tittle_scrn():
+    os.system('cls')
+    print("####################################")
+    print("#######  HANGMAN INDONESIA   #######")
+    print("####################################")
+    print("               - Play -             ")
+    print("               - Help -             ")
+    print("               - Quit -             ")
+    print("####################################")
+    print("####################################")
+    print("###  Created by : Faizar Rahman  ###")
+    print("###    Python Excercise 2020     ###")
+    print("####################################")
+    tittle_screen_selection()
+
+
+def help_menu():
+    os.system('cls')
+    print("####################################")
+    print("#######  HANGMAN INDONESIA   #######")
+    print("####################################")
+    print("     Tebak kata yang akan muncul    ")
+    print(" ketikkan 'huruf' atau 'kalimat' nya")
+    print("& kalian hanya memilki 5x kesempatan")
+    print("####################################")
+    print("####################################")
+    print("###  Created by : Faizar Rahman  ###")
+    print("###     Python Excercise 2020    ###")
+    print("####################################")
+    tittle_screen_selection()
+
+
+def tittle_screen_selection():
+    option = input("pilihan >").lower()
+    if option == ("play"):
+        main()
+    elif option == ("help"):
+        help_menu()
+    elif option == ("quit"):
+        print("oke,, sampai ketemu lagi!! ")
+        time.sleep(0.8)
+        sys.exit()
+
+
+def status_scrn(tries, word):
+    print("===========================")
+    print(f'sisa nyawa anda = {tries} ')
+    print(f'panjang huruf ' + str(len(word)))
+    print("===========================")
--- Hangman.py
+++ Hangman.py
@@ -1,14 +1,15 @@
-import random 
+import random
 from words import word_list
-from Display import display_hangman, status_scrn
-import sys 
-import os 
+import Display
+import sys
+import os
 import time
 
 
 def get_word():
     word = random.choice(word_list)
     return word.upper()
+
 
 def play(word):
     word_completion = "_" * len(word)
@@ -21,74 +22,69 @@ def play(word):
     os.system("cls")
     print(word)
     print("ayo main hangman!!!")
-    print(display_hangman(tries))
-    print (word_completion)
+    print(Display.display_hangman(tries))
+    print(word_completion)
     print("\n")
-    print(tries)
-    while not guessed and tries >0:
+    Display.status_scrn(tries, word)
+    # print(f'tries = {tries} ')
+    while not guessed and tries > 0:
         guess = input("ayo coba tebak kata atau hurufnya,,, ").upper()
-        if len(guess)==1 and guess.isalpha():
+        if len(guess) == 1 and guess.isalpha():
             if guess in guessed_letter:
                 print("udah pernah di coba,, coba yang lain" + guess)
             elif guess not in word:
-                print (guess + "ga ada dalam kata itu")
-                tries -=1 
+                print(guess + "ga ada dalam kata itu")
+                tries -= 1
                 guessed_letter.append(guess)
-            else : 
-                print ("keren..." + guess + "adalah hufur nya")
+            else:
+                print("keren..." + guess + "adalah hufur nya")
                 guessed_letter.append(guess)
                 word_as_list = list(word_completion)
-                indices = [i for i, letter in enumerate(word) if letter == guess]
-                for index in indices :
-                    word_as_list[index] = guess 
-                word_completion= "".join(word_as_list)
-                if "_" not in word_completion :
+                indices = [i for i, letter in enumerate(
+                    word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
                     guessed = True
-
-
-
-        elif len(guess)==len(word) or len(guess) != len(word) and guess.isalpha():
+        elif len(guess) == len(word) or len(guess) != len(word) and guess.isalpha():
             if guess.upper() == word.upper():
                 print("keeern" + guess + "adalah kata yang benar")
-                guessed = True 
-                word_completion = word  
+                guessed = True
+                word_completion = word
             elif guess not in guessed_words:
                 print("kata itu salah")
-                tries -=1 
+                tries -= 1
                 guessed_words.append(guess)
             elif guess in guessed_words:
-                print ("kata itu udah di coba")
+                print("kata itu udah di coba")
                 print(guessed_words)
-                             
 
         elif guess is not str:
-            print ("nit a valid guess...")
-
+            print("nit a valid guess...")
 
         time.sleep(0.5)
         os.system("cls")
         print("ayo main hangman!!!")
-        print(display_hangman(tries))
-        print (word_completion)
+        print(Display.display_hangman(tries))
+        print(word_completion)
         print("\n")
-        status_scrn()
-    if  guessed:
+        Display.status_scrn(tries, word)
+    if guessed:
         print("selamat!!!")
-    else :
-        print("orang nya kegantung,, anda gagal!!, kata yang benar adalah " + word + "coba lagi taun depan!!")
+    else:
+        print("orang nya kegantung,, anda gagal!!, kata yang benar adalah " +
+              word + "coba lagi taun depan!!")
 
-def main() : 
+
+def main():
     word = get_word()
     print(word)
     play(word)
-    while input("mau main lagi? (Y/N)" ).upper() == "Y":
+    while input("mau main lagi? (Y/N)").upper() == "Y":
         word = get_word()
         play(word)
 
-def status_scrn(): 
-    print("====================")
-    print(f"=== nyawa" + str(tries))
-    print("====================")
 
-if __name__ == "__main__" :
-    main()
+if __name__ == "__main__":
+    Display.tittle_scrn()
